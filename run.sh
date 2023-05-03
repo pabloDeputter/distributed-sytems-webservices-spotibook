ce8aacd57a1f87076473000363ca6ec2e6ef44df
pabloDeputter <pablo.deputter@student.uantwerpen.be>
2023-05-03 10:39:21 +0200

    update README

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# Start the microservices and user interface using docker-compose
+chmod +x ./activities_persistence/init.sh
+chmod +x ./friends_persistence/init.sh
+chmod +x ./playlists_persistence/init.sh
+chmod +x ./songs_persistence/init.sh
+chmod +x ./users_persistence/init.sh
+# Start the GUI microservice since this depends on all the other microservices.
 docker-compose up GUI --build
 
